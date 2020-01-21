85cfd41eb56dcbde66824a21c04130287cdc98ad
Iggr Smirnov <uchetzapis@yahoo.com>
2020-01-21 20:58:29 +0300

    add few routes

--- app.py
+++ app.py
@@ -1,21 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "main"
+    return render_template("index.html")
 
 
-@app.route("/goals/<goal>")
-def goal_v(goal):
-    return f"goal {goal}"
+@app.route("/goals/<goal>/")
+def goal_view(goal):
+    return render_template("goal.html", goal=goal)
 
 
-@app.route("/profiles/<int:id>")
+@app.route("/profiles/<int:id>/")
 def profile(teacher_id):
-    return f"iteacher id {teacher_id}"
+    return render_template("profile.html", id=teacher_id)
+
+
+@app.route('/request/')
+def request_view():
+    return render_template('pick.html')
+
+
+@app.route('/booking/<int:id>')
+def booking(id):
+    return render_template("booking.html", id=id)
 
 
 if __name__ == "__main__":
