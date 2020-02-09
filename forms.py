73a355ec2670ba635db7ce12b20cb2a0a51c7f32
Iggr Smirnov <uchetzapis@yahoo.com>
2020-02-09 19:28:16 +0300

    add phone check

--- forms.py
+++ forms.py
@@ -1,19 +1,30 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, RadioField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, ValidationError
+import phonenumbers
 
 
 class NamePhoneForm(FlaskForm):
     student_name = StringField("Вас зовут",
-                               validators=[DataRequired(message="заполни")])
+                               validators=[DataRequired(message="заполни")],
+                               render_kw={"class": "form-control"})
     student_phone = StringField("Ваш телефон",
                                 validators=[DataRequired(message="заполни"),
                                             Length(min=6,
                                             message="должен быть > 5 символов"
                                                    )
-                                            ]
+                                            ],
+                                render_kw={"class": "form-control"}
                                 )
     submit = SubmitField()
+
+    def validate_student_phone(form, field):
+        try:
+            input_number = phonenumbers.parse(field.data, 'RU')
+            if not (phonenumbers.is_valid_number(input_number)):
+                raise ValidationError("Некоректный номер")
+        except:
+            raise ValidationError("Некоректный номер")
 
 
 class RequestMatchingTeacherForm(NamePhoneForm):
@@ -40,4 +51,5 @@ class RequestMatchingTeacherForm(NamePhoneForm):
                                         ("5-7", "5-7 часов в неделю"),
                                         ("7-10", "7-10 часов в неделю")
                                         ],
-                               default="1-2")
+                               default="1-2",
+                               )
